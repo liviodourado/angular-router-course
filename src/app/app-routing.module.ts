f745bff1d32383c809f22753b0728f15e709067d
Livio Dourado <liviodourado@gmail.com>
2024-07-15 11:44:57 -0300

    feat: root route & page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { LoginComponent } from "./login/login.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/courses",
+    pathMatch: "full",
+  },
   {
     path: "courses",
     loadChildren: () =>
@@ -16,6 +22,10 @@ const routes: Routes = [
   {
     path: "about",
     component: AboutComponent,
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
   },
 ];
 
