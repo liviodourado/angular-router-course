f3e849a6a79e213172152c2560db81b94983c434
Livio Dourado <liviodourado@gmail.com>
2024-07-16 10:51:13 -0300

    feat: Route Snapshot x Route Observables

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { LessonDetail } from "../model/lesson-detail";
 
 @Component({
@@ -8,13 +10,25 @@ import { LessonDetail } from "../model/lesson-detail";
   styleUrls: ["./lesson-detail.component.css"],
 })
 export class LessonDetailComponent implements OnInit {
-  lesson: LessonDetail;
+  lesson$: Observable<LessonDetail>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     console.log("Created LessonDetailComponent...");
   }
 
   ngOnInit() {
-    this.lesson = this.route.snapshot.data["lesson"];
+    this.lesson$ = this.route.data.pipe(map((data) => data["lesson"]));
+  }
+
+  previous(lesson: LessonDetail) {
+    this.router.navigate(["lessons", lesson.seqNo - 1], {
+      relativeTo: this.route.parent,
+    });
+  }
+
+  next(lesson: LessonDetail) {
+    this.router.navigate(["lessons", lesson.seqNo + 1], {
+      relativeTo: this.route.parent,
+    });
   }
 }
